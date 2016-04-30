7b8a2509db00e81d0835e10a15a5e13e3d1a4c46
stepbot <stephanbotes@gmail.com>
2016-04-30 11:12:46 -0400

    initial map intergration

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,32 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET GeoJSON data. */
+router.get('/mapjson/:name', function (req, res) {
+    if (req.params.name) {
+        Json.findOne({ name: req.params.name },{}, function (err, docs) {
+            res.json(docs);
+        });
+    }
+});
+
+/* GET layers json data. */
+router.get('/maplayers', function (req, res) {
+    Json.find({},{'name': 1}, function (err, docs) {
+        res.json(docs);
+    });
+});
+
+/* GET Map page. */
+router.get('/map', function(req,res) {
+    var db = req.db;
+    Json.find({},{}, function(e,docs){
+        res.render('map', {
+            "jmap" : docs,
+            lat : 40.78854,
+            lng : -73.96374
+        });
+    });
+});
+
 module.exports = router;
